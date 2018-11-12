e69f2782285fecdb4d6bc4040ea0f0372834c4d4
Miguel Angel <miguelgalindez@unicauca.edu.co>
2018-11-12 10:25:06 -0500

    Splitting and combining reducers

--- ch5/4-bind-server.js
+++ ch5/4-bind-server.js
@@ -1,18 +1,10 @@
-/**
- * Creating a Redux store
- */
-
 const express=require('express')
 const path=require('path')
 const app=express()
-/**
- * Serving the Redux library on /lib path
- */
+
 app.use('/lib', express.static(path.join(__dirname, 'node_modules', 'redux', 'dist')))
 app.use('/lib', express.static(path.join(__dirname, 'node_modules')))
-
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '3-meal-time-client.html'))
+    res.sendFile(path.join(__dirname, '4-bind-index.html'))
 })
-
-app.listen(1337, ()=>console.log('Server running on port 1337'))
+app.listen(1337, ()=>console.log('Web server running on port 1337'))
--- ch5/5-todo-time.js
+++ ch5/5-todo-time.js
@@ -1,18 +1,10 @@
-/**
- * Creating a Redux store
- */
-
 const express=require('express')
 const path=require('path')
 const app=express()
-/**
- * Serving the Redux library on /lib path
- */
 app.use('/lib', express.static(path.join(__dirname, 'node_modules', 'redux', 'dist')))
 app.use('/lib', express.static(path.join(__dirname, 'node_modules')))
-
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '3-meal-time-client.html'))
+    res.sendFile(path.join(__dirname, '5-todo-time.html'))
 })
 
 app.listen(1337, ()=>console.log('Server running on port 1337'))
